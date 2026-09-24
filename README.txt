16c2e6a7dc09add9dbc656c3d1bc005a7dad4707
Benjamin Daines <benjamindaines@gmail.com>
2026-09-24 14:12:57 -0400

    Trying to clean this up to post a usable module....

--- README.txt
+++
@@ -1,1 +0,0 @@
-Work in progress. Currently jamming out to some fire tunes while I wait to see which services start back up. 
--- release/service.sh
+++
@@ -1,7 +0,0 @@
-#!/system/bin/sh
-
-MODDIR=${0%/*}
-LOG="$MODDIR/quiet.log"
-
-while [[ $(getprop service.bootanim.exit) != 1 ]]; do sleep 10; done
-/system/bin/sh minid.sh boot
